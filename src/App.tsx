4b7b9138a92400bfbe273ca2fb357299d67f573b
RamyaJarugulla7 <ramya.jarugulla@techdenali.com>
2026-06-19 17:22:44 +0530

    Initial widget implementation

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { WidgetOptions } from "./index.widget";
+
+interface AppProps {
+  options?: WidgetOptions;
+}
+
+const App: React.FC<AppProps> = ({ options }: any) => {
+
+  return (
+    <>
+      This is My Widget {options.name}
+    </>
+  )
+};
+
+export default App;
